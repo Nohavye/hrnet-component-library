05c029c17b6f6a282b85ffc1fc58effa1206a6a2
Nohavye <noha.poncelet@gmail.com>
2023-11-08 10:59:20 +0100

    Comments.

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,7 +1,7 @@
 import * as lib from '../lib/main'
 import * as mock from './mock'
 
-// Component to develop
+// Component to develop.
 export const TargetComponent = () => (
     <lib.DropdownSelector
         id={mock.DROPDOWN_SELECTOR.ID}
